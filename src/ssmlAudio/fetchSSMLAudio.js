3e7ccff1473d325eca0f7e4093cdd29f4a3f7fca
makersmelx <makersmelx@gmail.com>
2021-02-01 00:31:20 +0800

    add logger message for ssml audio delete/upload

--- src/ssmlAudio/fetchSSMLAudio.js
+++ src/ssmlAudio/fetchSSMLAudio.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import config from '../config';
 import urls from '../server/urls';
 import firebase from '../firebase';
+import logger from '../logger';
 
 const client = new TextToSpeechClient();
 const bucket = firebase.storage.bucket(config.bucketName);
@@ -19,9 +20,13 @@ const fetchSSMLAudio = async (name) => {
   if (audio.length > 0) {
     // delete current audio file if it exists
     const deleteFile = bucket.file(audio);
-    deleteFile.delete((err) => {
-      if (err) {
-        console.error(err);
+    // eslint-disable-next-line no-unused-vars
+    deleteFile.delete((errRes, apiResponse) => {
+      if (errRes) {
+        const errorMessage = errRes.errors[0].message;
+        logger.error(errorMessage);
+      } else {
+        logger.info(`Delete file ${path.join(config.storageBaseURL, audio)}`);
       }
     });
   }
@@ -35,12 +40,17 @@ const fetchSSMLAudio = async (name) => {
   const audioName = path.join(`${uuidv4()}.mp3`);
   const audioBucketPath = path.join(path.basename(urls.ssmlAudio), audioName);
   const file = bucket.file(audioBucketPath);
-  file.save(response.audioContent, (err) => {
-    if (err) {
-      console.error(err);
+  file.save(response.audioContent, (errRes) => {
+    if (errRes) {
+      const errorMessage = errRes.errors[0].message;
+      logger.error(errorMessage);
+    } else {
+      logger.info(
+        `Upload file at ${path.join(config.storageBaseURL, audioName)}`,
+      );
     }
   });
-  return Promise.resolve(path.join(urls.ssmlAudio, audioName));
+  return Promise.resolve(audioBucketPath);
 };
 
 export default {
