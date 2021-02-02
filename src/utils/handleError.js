a8f042c481fa63cb57415853faa6f3084012d0cb
makersmelx <makersmelx@gmail.com>
2021-02-03 00:17:27 +0800

    only uncaught error will be logged

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -16,21 +16,20 @@ export const handleError = (controller) => async (req, res, next) => {
   }
 };
 
-// eslint-disable-next-line no-unused-vars
 export const errorMiddleware = (err, req, res, next) => {
   // Handle errors here
   const isClientError = err instanceof CallMeError;
-  if (req.method !== 'GET') {
+  const httpStatus = isClientError ? err.code : 500;
+  // only uncaught error will be logged
+  if (!isClientError) {
     logger.error(err.stack);
   }
-  // if (process.env.NODE_ENV !== 'production') {
-  //   console.error(err.stack);
-  // }
-  res.status(isClientError ? err.code : 500).send(
+  res.status(httpStatus).send(
     {
       message: isClientError
         ? err.message
         : 'Something went wrong in the server.',
     },
   );
+  next();
 };
